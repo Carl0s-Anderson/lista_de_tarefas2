7b8888de02699b6e09a48a2e2fc93d95068b4788
Carl0s-Anderson <carlosanderson30030@gmail.com>
2026-03-28 16:38:43 -0300

    inseri registros e feadback

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -20,6 +20,12 @@
 		</div>
 	</nav>
 
+	<?php if (isset($_GET['sucesso']) && $_GET['sucesso'] == '1') { ?>
+        <div class="bg-success pt-2 text-white d-flex justify-content-center">
+            <h5>Tarefa inserida com sucesso!</h5>
+        </div>
+    <?php } ?>
+
 	<div class="container app">
 		<div class="row">
 			<div class="col-md-3 menu">
